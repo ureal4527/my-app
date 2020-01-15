7346a649c2dc5df1779442a5198838e4100650d9
ureal4527 <ureal4527@google.com>
2020-01-15 18:03:42 +0900

    https://github.com/ureal4527/my-app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
+//import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-   <h1>Hello!!! aaa</h1> 
-   <table>
-     <tr>
-       <td>a</td>
-       <td>a</td>
-       <td>a</td>
-       <td>a</td>
-       <td>s</td>
-     </tr>
-   </table>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+     isLoading: true,
+     movies: []
+    };
+    getMovies = async () => {
+      const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+      this.setState({movies, isLoading: false})
+      console.log(movies)
+    };
+    componentDidMount(){
+      this.getMovies();
+    }
+    render() {
+      const { isLoading, movies } = this.state;
+    return <div>{this.state.isLoading ? "Loading...": movies.map(movie =>  (
+      <Movie 
+      key={movie.id}
+      id={movie.id} 
+      year={movie.year} 
+      title={movie.title} 
+      summary={movie.summary} 
+      poster={movie.medium_cover_image}/>
+    ))}</div> 
+}
 }
 
 export default App;
